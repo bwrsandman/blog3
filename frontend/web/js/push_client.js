15d45ff87922fd2e79460b2a0e2ea0640830b555
Alex Sharov <www.pismeco@gmail.com>
2013-09-10 02:28:53 +0700

    sockets worked2

--- frontend/web/js/push_client.js
+++ frontend/web/js/push_client.js
@@ -1,4 +1,14 @@
-var socket = io.connect('http://blog3.ru:89');
-socket.on('add', function (data) {
-    console.log(data);
-});
+var news_socket = io.connect('http://blog3.ru:89');
+
+var news_events = {
+    notify: function (data) {
+        console.log(data);
+        var notify_container = $('<div class="alert"></div>');
+        notify_container.addClass('alert-'+ data.type);
+        $('.site-index:first').prepend(notify_container.text(data.message))
+    }
+};
+
+for (var i in news_events) {
+    news_socket.on(i,news_events[i]);
+}
