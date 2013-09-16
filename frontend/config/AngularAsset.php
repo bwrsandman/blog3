e87b2bd2c6deafe33d78107f85910b80a22ee632
Alex Sharov <www.pismeco@gmail.com>
2013-09-16 09:23:35 +0700

    bower components to component.json

--- frontend/config/AngularAsset.php
+++ frontend/config/AngularAsset.php
@@ -19,22 +19,22 @@ class AngularAsset extends AssetBundle
 	public $baseUrl = '@web';
 	public $js = array(
         '//maps.googleapis.com/maps/api/js?sensor=false',
-        'js/bower_components/angular/angular.js',
-//        'js/bower_components/angular-bootstrap/ui-bootstrap.js',
-//        'js/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+        'js/components/angular/angular.js',
+//        'js/components/angular-bootstrap/ui-bootstrap.js',
+//        'js/components/angular-bootstrap/ui-bootstrap-tpls.js',
         '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.4.0/ui-bootstrap-tpls.min.js',
-        'js/bower_components/angular-ui-router/release/angular-ui-router.min.js',
-//        'js/angular-modules/angular-translate.min.js',
-        'js/bower_components/angular-translate/angular-translate.js',
+        'js/components/angular-ui-router/release/angular-ui-router.min.js',
+//        'js/modules/angular-translate.min.js',
+        'js/components/angular-translate/angular-translate.js',
 
-        'js/angular-modules/app.js',
+        'js/modules/app.js',
 
-        'js/angular-modules/main/services/alert.js',
-        'js/angular-modules/main/controllers/root.js',
+        'js/modules/main/services/alert.js',
+        'js/modules/main/controllers/root.js',
 
-        'js/angular-modules/map/services/places.js',
-        'js/angular-modules/map/controllers/list.js',
-        'js/angular-modules/map/controllers/form.js',
-        'js/angular-modules/map/directives/pg-google-map.js',
+        'js/modules/map/services/places.js',
+        'js/modules/map/controllers/list.js',
+        'js/modules/map/controllers/form.js',
+        'js/modules/map/directives/pg-google-map.js',
     );
 }
