97a1ca48d54b5e60fe69c4bb302358cea76a19c4
Alex Sharov <www.pismeco@gmail.com>
2013-10-08 18:49:58 +0700

    reformat

---
+++ console/migrations/m131008_113758_create_table_goal.php
@@ -0,0 +1,25 @@
+<?php
+use yii\db\Schema;
+
+class m131008_113758_create_table_goal extends \yii\db\Migration
+{
+	public function up()
+	{
+        // MySQL-specific table options. Adjust if you plan working with another DBMS
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('goal', array(
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING.' NOT NULL',
+
+            'status' => 'tinyint NOT NULL DEFAULT 0',
+            'create_time' => Schema::TYPE_INTEGER.' NOT NULL',
+            'update_time' => Schema::TYPE_INTEGER.' NOT NULL',
+        ), $tableOptions);
+	}
+
+	public function down()
+	{
+        $this->dropTable('goal');
+	}
+}
