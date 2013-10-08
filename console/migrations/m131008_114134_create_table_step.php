6f2d5b887c8ef0cc3e3b259f6396f7be7fe7350f
Alex Sharov <www.pismeco@gmail.com>
2013-10-08 18:49:58 +0700

    reformat

---
+++ console/migrations/m131008_114134_create_table_step.php
@@ -0,0 +1,30 @@
+<?php
+use yii\db\Schema;
+
+class m131008_114134_create_table_step extends \yii\db\Migration
+{
+    public function up()
+    {
+        // MySQL-specific table options. Adjust if you plan working with another DBMS
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('step', array(
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'fk_goal' => Schema::TYPE_INTEGER . ' NOT NULL',
+
+            'status' => 'tinyint NOT NULL DEFAULT 0',
+            'create_time' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'update_time' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ), $tableOptions);
+
+        $this->addForeignKey('steps_to_goal', 'step', 'fk_goal', 'goal', 'id');
+
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('steps_to_goal', 'step');
+        $this->dropTable('step');
+    }
+}
