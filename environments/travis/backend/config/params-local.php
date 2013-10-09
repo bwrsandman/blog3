7969ec6b01ca735342ec8c4a7993728da3ebff86
Alex Sharov <www.pismeco@gmail.com>
2013-10-09 16:10:49 +0700

    travis env add

---
+++ environments/travis/backend/config/params-local.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'components.db' => array(
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=blog3',
+        'username' => 'travis',
+        'charset' => 'utf8',
+    ),
+);
---
+++ environments/travis/common/config/params-local.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'components.db' => array(
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=blog3',
+        'username' => 'root',
+        'password' => 'asharov',
+        'charset' => 'utf8',
+    ),
+);
