e43e254fb984a62c21b69870e31f0d670d6a1fbb
Alex Sharov <www.pismeco@gmail.com>
2013-11-12 17:47:56 +0700

    cleanup

--- frontend/config/AngularAsset.php
+++ frontend/config/AngularAsset.php
@@ -9,10 +9,6 @@ namespace frontend\config;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AngularAsset extends AssetBundle
 {
 	public $basePath = '@webroot';
