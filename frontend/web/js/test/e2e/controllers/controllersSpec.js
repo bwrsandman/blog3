c149a5597e4a8f3c80726c0138128a75971465e6
Alex Sharov <www.pismeco@gmail.com>
2013-11-13 10:59:13 +0700

    karma start

--- frontend/web/js/test/e2e/controllers/controllersSpec.js
+++ frontend/web/js/test/e2e/controllers/controllersSpec.js
@@ -6,12 +6,23 @@ describe("E2E: Testing Controllers", function () {
     beforeEach(function () {
         browser().navigateTo('/');
     });
-
-    it('should have a working videos page controller that applies the videos to the scope', function () {
-
+    it('home link must be empty', function() {
         browser().navigateTo('/');
         expect(browser().location().path()).toBe("/");
         expect(element('ng-view').html()).toContain('Сделано за сегодня');
+    });
+
+    it('Navigation click must change url', function () {
+        element('.test-navigation a:eq(1)').click();
+        expect(browser().location().path()).toBe("/");
+        element('.test-navigation a:eq(2)').click();
+        expect(browser().location().path()).toBe("/yesterday");
+        element('.test-navigation a:eq(3)').click();
+        expect(browser().location().path()).toBe("/wall");
+        element('.test-navigation a:eq(4)').click();
+        expect(browser().location().path()).toBe("/stream");
+        element('.test-navigation a:eq(5)').click();
+        expect(browser().location().path()).toBe("/history");
     });
 
 });
