119480c3d33dc4b67ca86937bf47e62c5684b3bf
Alex Sharov <www.pismeco@gmail.com>
2013-11-28 13:27:29 +0700

    add reason of goal

--- frontend/web/js/test/e2e/controllers/controllersSpec.js
+++ frontend/web/js/test/e2e/controllers/controllersSpec.js
@@ -2,17 +2,14 @@
 // test/e2e/controllers/controllersSpec.js
 //
 describe("E2E: Testing Controllers", function () {
-    var $scope, $httpBackend, $ctrl;
 
-    beforeEach(inject(function(GoalCtrl) {
-        $scope = GoalCtrl.$scope;
-        $ctrl = GoalCtrl.$scope;
-    }));
-
+    // Load the AngularJS homepage.
     beforeEach(function () {
-        browser().navigateTo('/');
+        browser().navigateTo('/?id=1');
     });
 
+
+    /*
     it('Home link must be empty', function () {
         browser().navigateTo('/');
         expect(browser().location().path()).toBe("/");
@@ -31,9 +28,20 @@ describe("E2E: Testing Controllers", function () {
         element('.test-navigation a:eq(5)').click();
         expect(browser().location().path()).toBe("/history");
     });
+    */
 
     it('Must save everything', function () {
-
+        var $messages = [];
+        for (var i = 0; i < 10; i++) {
+            $messages[i] = i + ' Hello ' + Math.random();
+            var input = element("#goals-grid textarea:eq("+i+")");
+            input.val($messages[i]);
+            input.trigger('change');
+        }
+        sleep(2)
+        browser().navigateTo('/?id=1');
+        for (var i = 0; i < 10; i++) {
+            expect(element("#goals-grid textarea:eq("+i+")").val()).toEqual($messages[i]);
+        }
     });
-
 });
