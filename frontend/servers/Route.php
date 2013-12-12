7e3264c9924b58cfebcaa9112f7c2e86201ae7e3
Alex Sharov <www.pismeco@gmail.com>
2013-12-12 18:22:27 +0700

    use versioning, and clenup

--- frontend/servers/Route.php
+++ frontend/servers/Route.php
@@ -3,8 +3,4 @@ namespace frontend\servers;
 
 class Route extends \nizsheanez\websocket\Route
 {
-    public function onFrame($message, $type)
-    {
-        parent::onFrame($message, $type);
-    }
 }
--- frontend/servers/WebSocket.php
+++ frontend/servers/WebSocket.php
@@ -5,10 +5,5 @@ class WebSocket extends \nizsheanez\websocket\Server
 {
     public $routeClass = '\frontend\servers\Route';
 
-    public function onReady()
-    {
-        parent::onReady();
-    }
-
 }
 
