7eddf9daf75d07349f26440221dfbd3612d3750d
Alexey Sharov <www.pismeco@gmail.com>
2014-03-12 11:49:42 +0700

    start hooks

--- frontend/modules/gitHooks/controllers/SubtreeUpdateController.php
+++ frontend/modules/gitHooks/controllers/SubtreeUpdateController.php
@@ -1,23 +1,45 @@
 <?php
 namespace frontend\modules\gitHooks\controllers;
 
+use yii\helpers\Html;
 use yii\web\Controller;
 use Yii;
 
-class SubtreeUpdateController extends Controller {
+class SubtreeUpdateController extends Controller
+{
 
     protected $repoDir = '/var/www/repos/eg';
+    protected $binDir = '/var/www/bin';
     protected $map = [
         'extensions/yii2-html-encode-behavior' => 'git@github.com:nizsheanez/yii2-html-encode-behavior'
     ];
 
     public function actionIndex()
     {
-        $splitScript = $this->repoDir . '/extensions/git-simple-split.sh';
+        $currentDir  = getcwd();
+        $splitScript = Yii::getAlias('@ext') . '/git-simple-split.sh';
+        $upCmd       = 'git checkout master && git reset --hard && git pull';
+
+        chmod($splitScript, 0776);
         foreach ($this->map as $prefix => $repo) {
+            chdir($this->repoDir);
+            $this->runCommand($upCmd);
             $cmd = $splitScript . ' ' . $prefix . ' ' . $repo;
-            var_dump(exec($cmd, $output));
-            print_r($output);
+
+            $this->runCommand($cmd);
         }
+
+        chdir($currentDir);
+    }
+
+    protected function runCommand($command)
+    {
+        exec($command, $out);
+        $this->echoPre("command: " . $command . "\n" . "Out: " . print_r($out, true));
+    }
+
+    protected function echoPre($str)
+    {
+        echo Html::tag('pre', $str);
     }
 }
