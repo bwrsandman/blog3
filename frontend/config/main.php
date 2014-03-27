b12e2f8e7fec438c79d70ce6d24042c8fc77c5fb
Alexey Sharov <www.pismeco@gmail.com>
2014-03-27 18:18:34 +0700

    new Yii2 loging logic support

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -90,18 +90,6 @@ return array(
                 'force'   => true
             ]
         ],
-        'log'          => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets'    => [
-                [
-                    'class'  => 'yii\log\FileTarget',
-                    'levels' => [
-                        'error',
-                        'warning'
-                    ],
-                ],
-            ],
-        ],
     ],
     'params'              => $params,
 );
